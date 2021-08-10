4943f16f67f4974865dff1f062782cf3141cb07c
ignatandrei <ignatandrei@yahoo.com>
2021-08-10 19:24:07 +0300

    configurator

---
+++ src/ConfigureMS/RepoMSFile.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+//using Newtonsoft.Json;
+
+namespace ConfigureMS
+{
+    public class RepoMSFile : IRepoMS
+    {
+        private readonly string fileName;
+        private readonly IFileSystem fileSystem;
+        private readonly int lastDot;
+
+        public RepoMSFile(string fileName, IFileSystem fileSystem)
+        {
+            this.fileName = fileName;
+            this.fileSystem = fileSystem;
+            this.lastDot = fileName.LastIndexOf("."); 
+        }
+        public static string GetFriendlyName(Type type)
+        {
+            string friendlyName = type.Name;
+            if (type.IsGenericType)
+            {
+                int iBacktick = friendlyName.IndexOf('`');
+                if (iBacktick > 0)
+                {
+                    friendlyName = friendlyName.Remove(iBacktick);
+                }
+                friendlyName += "<";
+                Type[] typeParameters = type.GetGenericArguments();
+                for (int i = 0; i < typeParameters.Length; ++i)
+                {
+                    string typeParamName = GetFriendlyName(typeParameters[i]);
+                    friendlyName += (i == 0 ? typeParamName : "," + typeParamName);
+                }
+                friendlyName += ">";
+            }
+
+            return friendlyName;
+        }
+        private string AppendType(Type t)
+        {
+            string fileForType = fileName + "." + GetFriendlyName( t);
+            if (lastDot > 0)
+                fileForType = fileName.Substring(0, lastDot) + GetFriendlyName(t) + "." + fileName.Substring(lastDot);
+            return fileForType.Replace("<","_").Replace(">","_");
+        }
+        public async Task<T> GetItem<T>()
+        {
+
+            var data = await fileSystem.File.ReadAllTextAsync(AppendType(typeof(T)));
+            
+            //          var obj = JsonConvert.DeserializeObject(data,
+            //typeof(T),
+            //new JsonSerializerSettings()
+            //{
+            //    TypeNameHandling = TypeNameHandling.Auto,
+            //    Formatting = Formatting.Indented
+            //}) ;
+            //return (T)obj ;
+            //System.Text.Json cannot serialize properly interfaces
+            return JsonSerializer.Deserialize<T>(data);
+
+
+        }
+
+        public async Task<int> SaveData<T>(T t)
+        {
+            ////System.Text.Json cannot serialize properly interfaces
+            var data = JsonSerializer.Serialize(t,
+                
+                new JsonSerializerOptions()
+                {
+                    WriteIndented =true
+                });
+  //          var data= JsonConvert.SerializeObject(
+  //t,
+  //typeof(T),
+  //new JsonSerializerSettings()
+  //{
+  //    TypeNameHandling = TypeNameHandling.Auto,
+  //    Formatting = Formatting.Indented,
+  //});
+            await fileSystem.File.WriteAllTextAsync(AppendType(typeof(T)),data);
+            return data.Length;
+        }
+
+
+    }
+}
---
+++ src/ConfigureMS/StartConfigurationMS.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace ConfigureMS
+{
+    public class SHIM_StartConfigurationMS : IStartConfigurationMS
+    {
+        //TODO: use RSCG
+        public SHIM_StartConfigurationMS()
+        {
+
+        }
+        public SHIM_StartConfigurationMS(IStartConfigurationMS configure)
+        {
+            this.Name = configure.Name;
+            this.ConfiguredAt = configure.ConfiguredAt;
+            this.ChoosenMainProvider = configure.ChoosenMainProvider;
+            this.MainProviders = configure.MainProviders;
+            this.BaseFolder = configure.BaseFolder;
+            this.ChoosenProviderData = configure.ChoosenProviderData;
+        }
+        public Task<bool> IsComplete()
+        {
+            foreach (var item in Validate(null))
+            {
+                return Task.FromResult(false);
+            }
+            return Task.FromResult(true);
+
+        }
+        public bool IsConfigured() => ConfiguredAt != null;
+         
+        public string Name { get; set; }
+
+        public DateTime? ConfiguredAt { get; set; }
+
+        public string BaseFolder
+        {
+            get;
+        }
+        public string ChoosenMainProvider { get; set; }
+
+
+        public string[] MainProviders { get; set; }
+
+        public IData ChoosenProviderData { get; set; }
+
+        public Task<int> ChooseConfiguration(string name, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> ConfigureAgain()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> LoadConfiguration()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> LoadData(IRepoMS repo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveData(IRepoMS repo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncEnumerable<ValidationResult> StartFinding(string baseDir)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            yield break;
+        }
+    }
+    public interface IStartConfigurationMS:ISaveAndLoadData, IValidatableObject        
+    {
+        string Name { get;}
+        bool IsConfigured()=> ConfiguredAt != null;
+        public string BaseFolder
+        {
+            get;
+        }
+        DateTime? ConfiguredAt { get; set; }
+        public string ChoosenMainProvider { get; set; }
+        IAsyncEnumerable<ValidationResult> StartFinding(string baseDir);
+
+        //Task<bool> IsComplete();
+
+        Task<int> ChooseConfiguration(string name, string value);
+        Task<int> ConfigureAgain();
+
+        public string[] MainProviders { get; }
+
+        public IData ChoosenProviderData { get; }
+
+    }
+    public interface ISaveAndLoadData
+    {
+        Task<int> SaveData(IRepoMS repo);
+        Task<int> LoadData(IRepoMS repo);
+    }
+     
+    public interface IRepoMS
+    {
+        Task<T> GetItem<T>();
+        Task<int> SaveData<T>(T t);
+    }
+    public interface IData : ISaveAndLoadData
+    {
+
+        //string SavedData { get; set; }
+        
+        public IDictionary<string, object> WriteProperties();
+        public IDictionary<string, object> ReadProperties(); 
+        public void SetProperties(IDictionary<string, object> values);
+        public Task Test();
+    }
+    public interface IConfigurableMS {
+        string Name { get; set; }
+        string Type{ get;  }
+
+        string Description { get; }
+    }
+}
